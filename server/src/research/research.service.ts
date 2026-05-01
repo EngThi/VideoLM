4172863ca6de432675f56368f083624096d7528a
stevebatista120 <stevebatista120@gmail.com>
2026-05-01 17:45:12 +0000

    Reduce infographic branding overlay footprint

--- server/src/research/research.service.ts
+++ server/src/research/research.service.ts
@@ -58,9 +58,9 @@ export class ResearchService {
 
     const ext = path.extname(inputPath);
     const tempOutput = inputPath.replace(new RegExp(`${ext}$`), `.branded${ext}`);
-    const overlayWidth = artifactType === 'video' ? 180 : 280;
+    const overlayWidth = artifactType === 'video' ? 180 : 250;
     const overlayMarginX = artifactType === 'video' ? 18 : 6;
-    const overlayMarginY = artifactType === 'video' ? 18 : 0;
+    const overlayMarginY = artifactType === 'video' ? 18 : 6;
     const overlayFilter = `[1:v]scale=${overlayWidth}:-1[wm];[0:v][wm]overlay=main_w-overlay_w-${overlayMarginX}:main_h-overlay_h-${overlayMarginY}:shortest=1[v]`;
 
     const args = artifactType === 'video'
